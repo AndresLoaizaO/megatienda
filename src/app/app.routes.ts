1632a1237fedb6c7e27e00c868f73d168b23bcf6
jhon_guevara <jhonguevara1401@gmail.com>
2026-02-28 23:34:30 -0500

    Creacion de rutas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { ProductCatalogComponent } from './modules/products/product-catalog/product-catalog.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  //{ path: '', component: HomeComponent },       // ruta raíz
+  //{ path: 'about', component: AboutComponent }, // ruta /about
+  { path: 'catalogo', component: ProductCatalogComponent },       // ruta del catalogo
+  { path: '**', redirectTo: '' }                // ruta por defecto (404)
+];
+
