bbed77ea75cfbdf3e29b6fd4cc5c5b93cc423f85
jhon_guevara <jhonguevara1401@gmail.com>
2026-03-01 01:57:53 -0500

    Creacion de tarjeta

---
+++ src/app/modules/products/interface/interfaceProduct.ts
@@ -0,0 +1,7 @@
+export interface interfaceProduct {
+  name: string;
+  categoria: string;
+  precio: number;
+  rating: number;
+  imagenUrl: string;
+}
--- src/app/modules/products/product-view/product-view.component.ts
+++ src/app/modules/products/product-view/product-view.component.ts
@@ -1,11 +1,26 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {Rating} from 'primeng/rating';
+import {Button} from 'primeng/button';
+import {FormsModule} from '@angular/forms';
+import {Card} from 'primeng/card';
+import {interfaceProduct} from '../interface/interfaceProduct';
+import {SharedModule} from 'primeng/api';
 
 @Component({
   selector: 'app-product-view',
   standalone: true,
-  imports: [CommonModule],   // para usar directivas como *ngIf, *ngFor
+  imports: [CommonModule, Rating, Button, FormsModule, Card, SharedModule],   // para usar directivas como *ngIf, *ngFor
   templateUrl: './product-view.component.html',
-  styleUrls: ['./product-view.component.css']
+  styleUrls: ['./product-view.component.scss']
 })
-export class ProductViewComponent { }
+export class ProductViewComponent {
+// En tu componente .ts
+  producto: interfaceProduct = {
+    name: 'Smartwatch Series S',
+    categoria: 'Accesorios',
+    precio: 85.50,
+    rating: 5,
+    imagenUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcScLM8IP85y-sXW1OvJ1vvtQadLIsK5FxSakg&s'
+  };
+}
